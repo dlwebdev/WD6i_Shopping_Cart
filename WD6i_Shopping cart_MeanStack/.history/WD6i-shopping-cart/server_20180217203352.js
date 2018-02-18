d573889b9ba581187f66c5ce4707345b55987615
Justin Weber <jjweber@fullsail.edu>
2018-02-17 23:03:03 -0500

    Moving work to laptop

---
+++ WD6i_Shopping cart_MeanStack/.history/WD6i-shopping-cart/server_20180217203352.js
@@ -0,0 +1,17 @@
+
+
+
+/*
+
+// Connect To Database (OLD CODE)
+mongoose.connect(config.database);
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to Database '+config.database);
+});
+// On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error '+err);
+});
+
+*/
